eea6307734af4042e7f6b2ff79e354cc4848402c
iwscream <874223998>
2021-01-28 23:38:28 +0800

    first commit

---
+++ out/artifacts/push_services_war_exploded/lib/js/ScreenDirectionListener.js
@@ -0,0 +1,42 @@
+let detectorDisplayed = false;
+let inputFocus = false;
+
+$(document).ready(function() {
+    $("#loading").fadeIn(0);
+    function detectMob() {
+        return (navigator.userAgent.match(/Android/i)
+            || navigator.userAgent.match(/webOS/i)
+            || navigator.userAgent.match(/iPhone/i)
+            || navigator.userAgent.match(/iPad/i)
+            || navigator.userAgent.match(/iPod/i)
+            || navigator.userAgent.match(/BlackBerry/i)
+            || navigator.userAgent.match(/Windows Phone/i));
+    }
+
+
+    let mql = window.matchMedia('(orientation: portrait)');
+    console.log(mql);
+    function handleOrientationChange(mql) {
+        if (inputFocus) return;
+        if (detectMob()) {
+            if(!mql.matches) {
+                if (!detectorDisplayed) $("#screen-direction-detector").fadeIn();  // 横屏
+                detectorDisplayed = true;
+            } else {
+                if (detectorDisplayed) $("#screen-direction-detector").fadeOut();
+                detectorDisplayed = false;
+            }
+        } else {
+            $("#device-detector").fadeIn(0);
+        }
+    }
+
+    // 输出当前屏幕模式
+    handleOrientationChange(mql);
+    // 监听屏幕模式变化
+    mql.addListener(handleOrientationChange);
+});
+
+window.onload = function() {
+    $("#loading").fadeOut();
+};
